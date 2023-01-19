af7d9d97ecd0d6af1186242e548c06a91dc1f240
Victor Matheus <victormatheus183@gmail.com>
2023-01-19 20:50:15 -0300

    shotgun prefab created

---
+++ Assets/Scripts/Weapons/Guns/Shotgun.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Weapons.Guns
+{
+    public class Shotgun : GunBase
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            base.Start();
+        }
+
+
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
