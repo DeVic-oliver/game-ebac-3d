831716d6d2c93fc56f59393642b6e2f612715f4f
Victor Matheus <victormatheus183@gmail.com>
2023-01-20 15:35:29 -0300

    shotgun shoot behaviour implemented
    Shotgun fire more than one bullet. So its spawn and spread its bullets around the central bullet called in base.createbullet()

--- Assets/Scripts/Weapons/Guns/Shotgun.cs
+++ Assets/Scripts/Weapons/Guns/Shotgun.cs
@@ -1,23 +1,58 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Assets.Scripts.Weapons.Guns
 {
     public class Shotgun : GunBase
     {
+        [SerializeField] private float _spreadAngle;
+        [SerializeField] private float _bulletsQuantityOnShoot;
 
-        // Use this for initialization
-        void Start()
+        
+        protected override void CreateBullet()
         {
-            base.Start();
+            base.CreateBullet();
+
+            CreateSpreadedBullets();
         }
-
-
-
-        // Update is called once per frame
-        void Update()
+        private void CreateSpreadedBullets()
         {
+            int spreadMultiplier = 0;
+            float spreadAngleAux = _spreadAngle;
 
+            for (int i = 1; i < _bulletsQuantityOnShoot; i++)
+            {
+                spreadMultiplier = IncrementMultiplierByIndexModZero(spreadMultiplier, i);
+                spreadAngleAux = TogglePositiveNegativeByIndexMod(spreadAngleAux, i);
+                ShotgunBulletsInstantiation(spreadAngleAux, spreadMultiplier);
+            }
+
+            Debug.Log($"All bullets shoot {_bulletsQuantityOnShoot}");
+        }
+        private int IncrementMultiplierByIndexModZero(int multiplier, int index)
+        {
+            if(index % 2 == 0)
+            {
+                return ++multiplier;
+            }
+
+            return multiplier;
+        }
+        private float TogglePositiveNegativeByIndexMod(float number, int index) 
+        {
+            if(index % 2 == 0)
+            {
+                return Math.Abs(number);
+            }
+
+            return number * -1;
+        }
+        private void ShotgunBulletsInstantiation(float spread, int spreadMultiplier)
+        {
+            var bullet = Instantiate(_bulletType);
+            bullet.transform.position = _gunBarrel.position;
+            bullet.transform.localEulerAngles = Vector3.zero + Vector3.up * (spread * spreadMultiplier);
         }
     }
 }
