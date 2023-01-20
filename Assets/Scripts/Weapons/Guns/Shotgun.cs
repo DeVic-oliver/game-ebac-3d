b64bc44e7a3344f6d213974d9998f3abbb6f3ac0
Victor Matheus <victormatheus183@gmail.com>
2023-01-20 18:24:20 -0300

    shotgun adjust

--- Assets/Scripts/Weapons/Guns/Shotgun.cs
+++ Assets/Scripts/Weapons/Guns/Shotgun.cs
@@ -6,29 +6,33 @@ namespace Assets.Scripts.Weapons.Guns
 {
     public class Shotgun : GunBase
     {
-        [SerializeField] private float _spreadAngle;
-        [SerializeField] private float _bulletsQuantityOnShoot;
+        [SerializeField] private float _spreadAngle = 5;
+        private float _bulletsQuantityOnShoot = 4;
 
         
         protected override void CreateBullet()
         {
-            base.CreateBullet();
-
+            CreateCentralBullet();
             CreateSpreadedBullets();
+        }
+        private void CreateCentralBullet()
+        {
+            var bullet = Instantiate(_bulletType, _gunBarrel);
+            bullet.transform.localPosition = _gunBarrel.localPosition;
+            bullet.transform.localRotation = _gunBarrel.localRotation;
+            bullet.transform.parent = null;
         }
         private void CreateSpreadedBullets()
         {
             int spreadMultiplier = 0;
             float spreadAngleAux = _spreadAngle;
 
-            for (int i = 1; i < _bulletsQuantityOnShoot; i++)
+            for (int i = 0; i < _bulletsQuantityOnShoot; i++)
             {
                 spreadMultiplier = IncrementMultiplierByIndexModZero(spreadMultiplier, i);
                 spreadAngleAux = TogglePositiveNegativeByIndexMod(spreadAngleAux, i);
                 ShotgunBulletsInstantiation(spreadAngleAux, spreadMultiplier);
             }
-
-            Debug.Log($"All bullets shoot {_bulletsQuantityOnShoot}");
         }
         private int IncrementMultiplierByIndexModZero(int multiplier, int index)
         {
@@ -50,9 +54,10 @@ namespace Assets.Scripts.Weapons.Guns
         }
         private void ShotgunBulletsInstantiation(float spread, int spreadMultiplier)
         {
-            var bullet = Instantiate(_bulletType);
-            bullet.transform.position = _gunBarrel.position;
+            var bullet = Instantiate(_bulletType, _gunBarrel);
+            bullet.transform.localPosition = _gunBarrel.localPosition;
             bullet.transform.localEulerAngles = Vector3.zero + Vector3.up * (spread * spreadMultiplier);
+            bullet.transform.parent = null;
         }
     }
 }
