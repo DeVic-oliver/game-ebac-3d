e57b9faa4a6ee9e812e63ffb1bcec517447f677b
Victor Matheus <victormatheus183@gmail.com>
2023-02-01 22:04:52 -0300

    Acid Spit type projectile

---
+++ Assets/Scripts/Enemies/Blob/BloblProjectiles/AcidSpit.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Scripts.Core.Components.Projectile;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Enemies.Blob.BloblProjectiles
+{
+    public class AcidSpit : Projectile
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            base.Update();
+        }
+    }
+}
