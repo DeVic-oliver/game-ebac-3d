571b71dfb08eb1d10563504a7e16221130aadfe7
Victor Matheus <victormatheus183@gmail.com>
2023-02-01 22:05:26 -0300

    Blobl ranged boss implementation

---
+++ Assets/Scripts/Enemies/Blob/Boss/RangedBlobBoss.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using Assets.Scripts.Core.Enemies;
+using Assets.Scripts.Core.Interfaces;
+
+namespace Assets.Scripts.Enemies.Blob
+{
+    public class RangedBlobBoss : RangedEnemy, IMoveable, IDamageable
+    {
+        public void Move(bool isAlive)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void TakeDamage(int damageValue)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+    }
+}
