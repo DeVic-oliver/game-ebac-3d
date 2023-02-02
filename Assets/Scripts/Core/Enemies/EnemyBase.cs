108078a50e8a002cde54ecfade4d5d6628e638e3
Victor Matheus <victormatheus183@gmail.com>
2023-02-01 21:57:43 -0300

    New abstraction for type of enemies

---
+++ Assets/Scripts/Core/Enemies/EnemyBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Core.Enemies
+{
+    public abstract class EnemyBase : MonoBehaviour
+    {
+        public bool IsAlive { get; private set; }
+        public bool IsAttacking { get; private set; }
+
+        [Header("Health")]
+        [SerializeField] private int _healthPoints = 5;
+
+        [Header("Damage settings")]
+        [SerializeField] private MeshRenderer _enemyMeshRenderer;
+        [SerializeField] private Color _damageFeedbackColor;
+        [SerializeField] private float _flashSpeed = 1f;
+        [SerializeField] private ParticleSystem _damageVFX;
+
+        protected virtual void PlayDamageVfx()
+        {
+            if (_damageVFX != null)
+            {
+                _damageVFX.Play();
+            }
+        }
+    }
+}
