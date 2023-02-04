83078fe566e4ceb4571eadb35c0cb1c545c0c110
Victor Matheus <victormatheus183@gmail.com>
2023-02-04 17:03:59 -0300

    IPushable implementation
    Melee blob are pushable enemies

--- Assets/Scripts/Enemies/Blob/Minion/MeleeBlobMinion.cs
+++ Assets/Scripts/Enemies/Blob/Minion/MeleeBlobMinion.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 using Assets.Scripts.Enemies;
 using Assets.Scripts.Core.Enemies;
 using Assets.Scripts.Core.Interfaces;
+using DG.Tweening;
 
 namespace Assets.Scripts.Enemies.Blob
 {
 
-    public class MeleeBlobMinion : MeleeEnemy, IDamageable, IMoveable
+    public class MeleeBlobMinion : MeleeEnemy, IDamageable, IMoveable, IPushable
     {
+
         new void Start()
         {
             base.Start();
@@ -20,10 +22,12 @@ namespace Assets.Scripts.Enemies.Blob
             base.Update();
             Move(IsAlive);
         }
+     
 
         public void TakeDamage(int damageValue)
         {
             DecreaseHealthByDamageWithFlashFeedback(damageValue);
+            PlayDamageComponentVFX("bloodSpill");
         }
 
         public void Move(bool isAlive)
@@ -33,7 +37,12 @@ namespace Assets.Scripts.Enemies.Blob
                 MoveTowardsEnemy();
             }
         }
-       
+
+        public void Push(Vector3 force)
+        {
+            var forceVector = new Vector3(transform.localPosition.x - force.z, transform.position.y, transform.localPosition.z - force.z);
+            transform.DOMove(forceVector, .1f);
+        }
     }
 }
 
