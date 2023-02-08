48c5a5ca034d0fde7fc184bd2c9fbed804ec3439
Victor Matheus <victormatheus183@gmail.com>
2023-02-08 20:32:36 -0300

    Health funcionalities moved
    Health component handles only health logic. Health UI Manager handles with the renderization of health number to HUD

---
+++ Assets/Scripts/Core/Components/HealthUIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+namespace Assets.Scripts.Core.Components
+{
+    public class HealthUIManager : MonoBehaviour
+    {
+        [SerializeField] protected Image _fillableHealthBar;
+        [SerializeField] protected Health _health;
+
+
+        private void Update()
+        {
+            UpdateHealthBar();
+        }
+        private void UpdateHealthBar()
+        {
+            Debug.Log(GetHealthPercentage());
+            _fillableHealthBar.fillAmount = GetHealthPercentage();
+        }
+        private float GetHealthPercentage()
+        {
+            var percentage = (_health.CurrentHealth * 100f) / _health.GetTotalHealth();
+            percentage = GetNormalizedPercentage(percentage);
+            return percentage;
+        }
+        private float GetNormalizedPercentage(float value)
+        {
+            return value / 100f;
+        }
+    }
+}
