db8fcd406df08f6d92f3c1054bf29690df7e134b
Victor Matheus <victormatheus183@gmail.com>
2023-08-18 17:34:01 -0300

    Player animator handler for health params

---
+++ Assets/Scripts/Player/PlayerHealthAnimator.cs
@@ -0,0 +1,25 @@
+﻿namespace Assets.Scripts.Player
+{
+    using UnityEngine;
+
+    public class PlayerHealthAnimator : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+
+
+        public void SetIsAliveParamToFalse()
+        {
+            SetIsAliveParamTo(false);
+        }
+
+        public void SetIsAliveParamToTrue()
+        {
+            SetIsAliveParamTo(true);
+        }
+
+        private void SetIsAliveParamTo(bool isAlive)
+        {
+            _animator.SetBool("isAlive", isAlive);
+        }
+    }
+}
