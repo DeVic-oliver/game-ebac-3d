9c3828ec241f1a0e71a4be109ddaf68b047ea41e
Victor Matheus <victormatheus183@gmail.com>
2023-08-18 17:35:24 -0300

    A manager to deal with reload scene or rescue player to checkpoint

---
+++ Assets/Scripts/Core/PlayerDeathRescueManager.cs
@@ -0,0 +1,31 @@
+﻿namespace Assets.Scripts.Core
+{
+    using Assets.Scripts.Core.Components.Checkpoint;
+    using UnityEngine;
+    using UnityEngine.Events;
+    using UnityEngine.SceneManagement;
+    
+    public class PlayerDeathRescueManager : MonoBehaviour
+    {
+        public UnityEvent OnRescuingPlayer;
+        [SerializeField] private GameObject _player;
+        [SerializeField] private SceneCheckpointsManager _chekpointManager;
+
+
+        public void WhetherHasCheckpointRespawnPlayerOrReloadSceneIfNotHasCheckpoint()
+        {
+            if(SceneCheckpointsManager.CurrentCheckpointNumberActive > 0)
+            {
+                _player.SetActive(false);
+                OnRescuingPlayer?.Invoke();
+                _chekpointManager.RescuePlayerWhenItDies(_player.transform);
+                _player.SetActive(true);
+            }
+            else
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+
+    }
+}
