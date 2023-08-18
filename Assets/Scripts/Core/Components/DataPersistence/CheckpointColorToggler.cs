49b712670995f9382709c962ae1d1560ef444bd4
Victor Matheus <victormatheus183@gmail.com>
2023-08-18 12:08:48 -0300

    Cleaning "Checkpoint"
    CheckpointColorToggler created to handle with emission colors

---
+++ Assets/Scripts/Core/Components/DataPersistence/CheckpointColorToggler.cs
@@ -0,0 +1,37 @@
+﻿namespace Assets.Scripts.Core.Components.DataPersistence
+{
+    using UnityEngine;
+    
+    public class CheckpointColorToggler : MonoBehaviour
+    {
+        [SerializeField] private MeshRenderer _meshRenderer;
+        private Material _material;
+
+
+        public void TurnOnCheckpointColor()
+        {
+            SetMaterialEmissionColorToWhite();
+        }
+
+        private void SetMaterialEmissionColorToWhite()
+        {
+            _material.SetColor("_EmissionColor", Color.white);
+        }
+
+        public void TurnOffCheckpointColor()
+        {
+            SetMaterialEmissionColorToGray();
+        }
+
+        private void SetMaterialEmissionColorToGray()
+        {
+            _material.SetColor("_EmissionColor", Color.gray);
+        }
+
+        private void Awake()
+        {
+            _material = _meshRenderer.material;
+        }
+
+    }
+}
