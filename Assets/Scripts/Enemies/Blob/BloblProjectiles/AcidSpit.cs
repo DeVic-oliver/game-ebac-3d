159231d36dd089b929576849d8eba914e22c4eeb
Victor Matheus <victormatheus183@gmail.com>
2023-08-18 15:56:12 -0300

    Minor changes

--- Assets/Scripts/Enemies/Blob/BloblProjectiles/AcidSpit.cs
+++ Assets/Scripts/Enemies/Blob/BloblProjectiles/AcidSpit.cs
@@ -1,21 +1,12 @@
-﻿using Assets.Scripts.Core.Components.Projectile;
-using Assets.Scripts.Player;
-using System.Collections;
-using UnityEngine;
-
-namespace Assets.Scripts.Enemies.Blob.BloblProjectiles
+﻿namespace Assets.Scripts.Enemies.Blob.BloblProjectiles
 {
+    using Assets.Scripts.Core.Components.Projectile;
+    using Assets.Scripts.Player;
+    using UnityEngine;
+    
     public class AcidSpit : Projectile
     {
-
-        // Use this for initialization
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-       new void Update()
+        protected override void Update()
         {
             base.Update();
         }
