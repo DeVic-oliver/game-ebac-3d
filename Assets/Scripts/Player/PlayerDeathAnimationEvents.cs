1bb5124eeb3b66cc480e61a3294ff78848fc0900
Victor Matheus <victormatheus183@gmail.com>
2023-08-18 17:34:32 -0300

    Events handler for imported death animation

---
+++ Assets/Scripts/Player/PlayerDeathAnimationEvents.cs
@@ -0,0 +1,16 @@
+﻿namespace Assets.Scripts.Player
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+
+    public class PlayerDeathAnimationEvents : MonoBehaviour
+    {
+        public UnityEvent OnDeathKeyFrame;
+        
+        
+        public void InvokeEventsWhenReachesEventKeyFrame()
+        {
+            OnDeathKeyFrame?.Invoke();
+        }
+    }
+}
