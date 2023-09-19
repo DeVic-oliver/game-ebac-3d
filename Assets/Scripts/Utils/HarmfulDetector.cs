0c728884c8ca10c198caa1af1224b6ecfdfade33
Victor Matheus <matheuslima.o183@gmail.com>
2023-09-18 22:22:35 -0300

    Simple detector of game objects with tag harmful

---
+++ Assets/Scripts/Utils/HarmfulDetector.cs
@@ -0,0 +1,19 @@
+﻿namespace Assets.Scripts.Utils
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+
+    public class HarmfulDetector : MonoBehaviour
+    {
+        public UnityEvent OnHarmDetection;
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            GameObject gameObject = other.gameObject;
+            if (gameObject.CompareTag("Harmful"))
+                OnHarmDetection?.Invoke();
+        }
+
+    }
+}
