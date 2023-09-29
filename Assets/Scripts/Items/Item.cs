69510cd67e348f7dae9a7f44227d42c521b1f65a
Victor Matheus <matheuslima.o183@gmail.com>
2023-09-29 13:34:50 -0300

    plain class for Item objects

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,41 @@
+﻿namespace Assets.Scripts.Items
+{
+    using Assets.Scripts.Core.Enum.Item;
+    using Assets.Scripts.Core.ScriptableObjects;
+
+
+    public class Item
+    {
+        public int Quantity { get; private set; }
+
+        private ItemData _data;
+
+
+        public Item(ItemData data, int quantity)
+        {
+            _data = data;
+            Quantity = quantity;
+        }
+
+        public void IncrementQuantity()
+        {
+            Quantity++;
+        }
+
+        public void DecrementQuantity()
+        {
+            if(Quantity > 0)
+                Quantity--;
+        }
+
+        public int GetItemValue()
+        {
+            return _data.Value;
+        }
+
+        public ItemTypes GetItemType()
+        {
+            return _data.Type;
+        }
+    }
+}
