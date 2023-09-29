0c4535575476aa0356d0406c7767ee33530acf7e
Victor Matheus <matheuslima.o183@gmail.com>
2023-09-29 13:35:16 -0300

    Playerhealth consumes health coins in inventory

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,11 +1,16 @@
 ﻿namespace Assets.Scripts.Player
 {
     using Assets.Scripts.Core.Components;
+    using Assets.Scripts.Core.Enum.Item;
     using Assets.Scripts.Core.Interfaces;
-    
+    using Assets.Scripts.Items;
+    using UnityEngine;
 
     public class PlayerHealth : Health, IDamageable
     {
+        public Inventory Inventory;
+
+
         public void TakeDamage(int damageValue)
         {
             DecreaseHealth(damageValue);
@@ -19,6 +24,24 @@
         new void Update()
         {
             base.Update();
+
+            if (Input.GetKeyDown(KeyCode.R) && CurrentHealth < GetTotalHealth())
+            {
+                RecoverHealth();
+            }
+        }
+
+        private void RecoverHealth()
+        {
+            int newHealth = CurrentHealth + Inventory.UseItem(ItemTypes.HeatlhCoin);
+            if (newHealth > GetTotalHealth())
+            {
+                RestoreHealth();
+            }
+            else
+            {
+                CurrentHealth = newHealth;
+            }
         }
     }
 }
