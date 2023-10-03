921083d7aeae7bc7b5d38bc342fb1826cffb885b
Victor Matheus <matheuslima.o183@gmail.com>
2023-10-03 18:27:19 -0300

    TreasureChest classes created

---
+++ Assets/Scripts/Items/TreasureChestDrop.cs
@@ -0,0 +1,22 @@
+﻿namespace Assets.Scripts.Items
+{
+    using UnityEngine;
+    
+    public class TreasureChestDrop : MonoBehaviour
+    {
+        public GameObject ItemToDrop;
+        public int TreasureValue;
+
+        private int _quantityOfItemsToShowWhenOpened = 5;
+
+
+        public void DropItemWhenOpened()
+        {
+            for (int i = 0; i < _quantityOfItemsToShowWhenOpened; i++)
+            {
+                Instantiate(ItemToDrop, transform, false);
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Items/TreasureChestItem.cs
@@ -0,0 +1,21 @@
+﻿namespace Assets.Scripts.Items
+{
+    using System.Collections;
+    using UnityEngine;
+    
+    public class TreasureChestItem : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
