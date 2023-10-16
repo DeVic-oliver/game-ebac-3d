3072248393e06d2d857fe5f476b7d8f4c7558a26
Victor Matheus <matheuslima.o183@gmail.com>
2023-10-16 20:59:57 -0300

    Gateways to handle with powerup effects

---
+++ Assets/Scripts/Items/PowerUp/Gateway/ScaleGateway.cs
@@ -0,0 +1,33 @@
+﻿namespace Assets.Scripts.Items.PowerUp.Gateway
+{
+    using UnityEngine;
+    using System.Collections;
+    
+    public class ScaleGateway : MonoBehaviour
+    {
+        private Vector3 _defaultScale;
+        private Coroutine _currentCoroutine;
+
+        public void IncreaseScale(float value, float duration)
+        {
+            if(_currentCoroutine == null)
+            {
+                transform.localScale = Vector3.one * value;
+                IEnumerator numerator = ResetMoveSpeedAfterDurationEnds(duration);
+                _currentCoroutine = StartCoroutine(numerator);
+            }
+        }
+
+        private IEnumerator ResetMoveSpeedAfterDurationEnds(float duration)
+        {
+            yield return new WaitForSeconds(duration);
+            transform.localScale = _defaultScale;
+            _currentCoroutine = null;
+        }
+
+        private void Start()
+        {
+            _defaultScale = transform.localScale;
+        }
+    }
+}
