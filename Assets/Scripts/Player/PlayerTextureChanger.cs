1b86069bc4d5cb6e169336c5528350993e950707
Victor Matheus <matheuslima.o183@gmail.com>
2023-10-16 21:01:10 -0300

    A basic texture changer
    Changes the emission map

---
+++ Assets/Scripts/Player/PlayerTextureChanger.cs
@@ -0,0 +1,38 @@
+﻿namespace Assets.Scripts.Player
+{
+    using System.Collections;
+    using UnityEngine;
+    
+    public class PlayerTextureChanger : MonoBehaviour
+    {
+        [SerializeField] private Texture2D _defaultTexture;
+        [SerializeField] private Material _material;
+
+        private Coroutine _currentCoroutine;
+
+
+        public void ChangeTextureTo(Texture2D newTexture, float duration)
+        {
+            if (_currentCoroutine != null)
+                StopCurrentCoroutineThenSetItToNull();
+
+            _material.SetTexture("_EmissionMap", newTexture);
+
+            IEnumerator numerator = ResetToDefaultTextureAfterDuration(duration);
+            _currentCoroutine = StartCoroutine(numerator);
+        }
+
+        private void StopCurrentCoroutineThenSetItToNull()
+        {
+            StopCoroutine(_currentCoroutine);
+            _currentCoroutine = null;
+        }
+
+        private IEnumerator ResetToDefaultTextureAfterDuration(float duration)
+        {
+            yield return new WaitForSeconds(duration);
+            _material.SetTexture("_EmissionMap", _defaultTexture);
+        }
+
+    }
+}
