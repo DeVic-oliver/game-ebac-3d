ee387a221a96de3eeba19ce1dfc5a960bc190623
Victor Matheus <matheuslima.o183@gmail.com>
2023-10-16 21:00:14 -0300

    Power ups to apply some effect on player

---
+++ Assets/Scripts/Items/PowerUp/ScalePowerUp.cs
@@ -0,0 +1,18 @@
+﻿namespace Assets.Scripts.Items.PowerUp
+{
+    using Assets.Scripts.Items.PowerUp.Gateway;
+    using UnityEngine;
+    
+    public class ScalePowerUp : PowerUp
+    {
+        [SerializeField] private float _scaleMultiplier;
+
+
+        protected override void OnTriggerEnter(Collider other)
+        {
+            base.OnTriggerEnter(other);
+            if (other.TryGetComponent(out ScaleGateway scaleGateway))
+                scaleGateway.IncreaseScale(_scaleMultiplier, _duration);
+        }
+    }
+}
---
+++ Assets/Scripts/Items/PowerUp/SpeedPowerUp.cs
@@ -0,0 +1,18 @@
+﻿namespace Assets.Scripts.Items.PowerUp
+{
+    using Assets.Scripts.Items.PowerUp.Gateway;
+    using UnityEngine;
+    
+    public class SpeedPowerUp : PowerUp
+    {
+        [SerializeField] private float _speedValueToIncrease;
+
+        protected override void OnTriggerEnter(Collider other)
+        {
+            base.OnTriggerEnter(other);
+
+            if(other.TryGetComponent(out MoveSpeedGateway speedGateway))
+                speedGateway.AddValueToMoveSpeed(_speedValueToIncrease, _duration);
+        }
+    }
+}
