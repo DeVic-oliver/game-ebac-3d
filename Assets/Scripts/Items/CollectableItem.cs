063abed43acdbb05590bb0fee75aa2d58b4451b8
Victor Matheus <matheuslima.o183@gmail.com>
2023-11-11 16:17:42 -0300

    New UI assets added

--- Assets/Scripts/Items/CollectableItem.cs
+++ Assets/Scripts/Items/CollectableItem.cs
@@ -1,19 +1,51 @@
 ﻿namespace Assets.Scripts.Items
 {
     using Assets.Scripts.Core.ScriptableObjects;
+    using System.Collections;
     using UnityEngine;
 
     public class CollectableItem : MonoBehaviour
     {
         public ItemData Data;
 
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private Collider _collider;
+        [SerializeField] private MeshRenderer _mesh;
+
+        private Coroutine _currentCoroutine;
+
+
         protected void TryToGetGameObjectInventoryThenAddDataToItAndAutoDestroy(GameObject obj)
         {
             if(obj.TryGetComponent(out Inventory inventory))
             {
+                DisableColliderAndMeshComponents();
+                PlayAudio();
                 inventory.AddToInventory(Data);
-                Destroy(gameObject);
+
+                if(_currentCoroutine == null)
+                {
+                    _currentCoroutine = StartCoroutine(DestroyObjectAfter());
+                }
             }
         }
+
+        private void PlayAudio()
+        {
+            _audioSource.Play();
+        }
+
+        private void DisableColliderAndMeshComponents()
+        {
+            _collider.enabled = false;
+            _mesh.enabled = false;
+        }
+
+        private IEnumerator DestroyObjectAfter(float seconds = .5f)
+        {
+            yield return new WaitForSeconds(seconds);
+            Destroy(gameObject);
+        }
+
     }
 }
